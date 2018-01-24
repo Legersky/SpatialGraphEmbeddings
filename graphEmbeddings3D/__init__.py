228719e97b2f522a45b2b47c097cea548f39ff54
Legersky <jan.legersky@gmail.com>
2018-01-24 14:49:31 +0100

    sklearn to sys path

--- graphEmbeddings3D/__init__.py
+++ graphEmbeddings3D/__init__.py
@@ -6,12 +6,6 @@ if not os.path.isdir(os.path.dirname(os.path.realpath(__file__))+'/../results'):
     os.makedirs(os.path.dirname(os.path.realpath(__file__))+'/../results')
 
 import sys
-for i in sys.path:
-    print i
-print '-----------------'
 
 sys.path.insert(0,"/scratch/jlegersk/lib/python2.7/site-packages")
 sys.path.insert(0,"/scratch/jlegersk/lib/python2.7/site-packages/sklearn")
-for i in sys.path:
-    print i
-print '-----------------'
