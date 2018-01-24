192d54cd43bb6c0c021006d5666660612b25e57e
Legersky <jan.legersky@gmail.com>
2018-01-24 14:40:48 +0100

    sklearn to sys path

--- graphEmbeddings3D/__init__.py
+++ graphEmbeddings3D/__init__.py
@@ -4,3 +4,6 @@ if not os.path.isdir(os.path.dirname(os.path.realpath(__file__))+'/../tmp'):
     os.makedirs(os.path.dirname(os.path.realpath(__file__))+'/../tmp')
 if not os.path.isdir(os.path.dirname(os.path.realpath(__file__))+'/../results'):
     os.makedirs(os.path.dirname(os.path.realpath(__file__))+'/../results')
+
+import sys
+sys.path.insert(0,"/scratch/jlegersk/lib/python2.7/site-packages/sklearn/")
