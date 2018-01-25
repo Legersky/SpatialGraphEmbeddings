f3accb69af16c4ef845f698959c9c068b4dfd739
Legersky <jan.legersky@gmail.com>
2018-01-25 14:37:07 +0100

    licence statements added

--- graphEmbeddings3D/couplerCurve.py
+++ graphEmbeddings3D/couplerCurve.py
@@ -1,4 +1,4 @@
-#Copyright (C) 2018 Jan Legerský
+#Copyright (C) 2018 Jan Legersky
 #
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
