1e477e5c7a84a89a19ba41fea1fa3c87609b3cdc
Legersky <jan.legersky@gmail.com>
2018-01-25 08:28:54 +0100

    missing ..

--- sampling_scripts/random8vert160.py
+++ sampling_scripts/random8vert160.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 from graphEmbeddings3D.algRealEmbeddings import AlgRealEmbeddings
 from graphEmbeddings3D.graphEmbedding import getEdgeLengthsByEmbedding
 from random import uniform
--- sampling_scripts/rndSeven8vert160.py
+++ sampling_scripts/rndSeven8vert160.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 from graphEmbeddings3D.algRealEmbeddings import AlgRealEmbeddings
 from graphEmbeddings3D.graphEmbedding import getEdgeLengthsByEmbedding, GraphEmbedding
 from random import uniform
